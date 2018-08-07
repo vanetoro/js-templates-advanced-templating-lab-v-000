419bded00a2ce3568a8c4210b361274a5ad25717
Vanessa Toro <vanetoro84@gmail.com>
2018-08-07 17:17:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  var source = document.getElementById("recipe-form-template").innerHTML;
+  var template = Handlebars.compile(source)
+  document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
+
+  // template()
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
 
+function createRecipe(){
 
-Handlebars.registerPartial('recipePartial'.document.getElementById('recipeDetailsPartial').innerHTML)
+  // var ingredients =
+
+  var recipe = {name: $('#name').val(), description: $(`description`).val()}
+  var postHTML = template(recipe)
+
+  $('#main').append(postHTML)
+}
+
+
+// Handlebars.registerPartial('recipePartial'.document.getElementById('recipeDetailsPartial').innerHTML)
