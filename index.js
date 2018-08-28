74ef57f53cd6c1dc70136c2eb45cb321905de25c
Vanessa Toro <vanetoro84@gmail.com>
2018-08-28 19:31:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ function init() {
   //put any page initialization/handlebars initialization here
   var source = document.getElementById("recipe-form-template").innerHTML;
   var template = Handlebars.compile(source)
+<<<<<<< HEAD
   // debugger
   document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
 
 
+=======
+  document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
+
+  // template()
+>>>>>>> abaddf0397bd2516e6e4f7fa5a2ba91a42e3448b
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
@@ -13,6 +19,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 function createRecipe(){
 
+<<<<<<< HEAD
   var recipe = document.getElementById("recipe-template").innerHTML
   var template = Handlebars.compile(recipe)
   var recipeName = document.getElementById('name').value
@@ -51,5 +58,15 @@ function updateRecipe(){
 
 
 
+=======
+  // var ingredients =
+
+  var recipe = {name: $('#name').val(), description: $(`description`).val()}
+  var postHTML = template(recipe)
+
+  $('#main').append(postHTML)
+}
+
+>>>>>>> abaddf0397bd2516e6e4f7fa5a2ba91a42e3448b
 
 // Handlebars.registerPartial('recipePartial'.document.getElementById('recipeDetailsPartial').innerHTML)
